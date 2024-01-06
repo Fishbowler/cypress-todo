fd038c150d3333dd01e3a3ab3de1c469ca71d83f
Dan Caseley <dan@caseley.me.uk>
2024-01-06 22:05:28 +0000

    Fix config to allow threshold in visual regression testing

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -21,10 +21,7 @@ import './commands'
 
 import {addMatchImageSnapshotCommand} from '@simonsmith/cypress-image-snapshot/command'
 
-addMatchImageSnapshotCommand()
-
-// can also add any default options to be used
-// by all instances of `matchImageSnapshot`
 addMatchImageSnapshotCommand({
-  failureThreshold: 0.2,
+    failureThreshold: 0.01,
+    failureThresholdType: 'percent',
 })

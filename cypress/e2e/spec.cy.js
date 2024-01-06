ce93382ba3b3446a1c29e16ac9561ce36542fbf1
Dan Caseley <dan@caseley.me.uk>
2024-01-06 23:20:35 +0000

    Add list filtering tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -157,3 +157,61 @@ describe('todolist list CRUD', () => {
     listpage.getItem("my fifth task").should('be.visible')
   })
 })
+
+describe('todolist filtering', () => {
+  it('can filter by active tasks', () => {
+    cy.visit('/todolists.html')
+
+    const listspage = new Listspage()
+    
+    listspage.createList("another list")
+    listspage.openList("another list")
+
+    const listpage = new Listpage()
+
+    listpage.createItem("my first task")
+    listpage.createItem("my second task")
+    listpage.createItem("my third task")
+    listpage.createItem("my fourth task")
+    listpage.createItem("my fifth task")
+
+    listpage.toggleItem("my second task")
+    listpage.toggleItem("my fourth task")
+
+    listpage.elements.activeFilter().click()
+
+    listpage.getItem("my first task").should('be.visible')
+    listpage.getItem("my third task").should('be.visible')
+    listpage.getItem("my fifth task").should('be.visible')
+    listpage.elements.listOfItems().should('not.contain.text', "my second task")
+    listpage.elements.listOfItems().should('not.contain.text', "my fourth task")
+  })
+
+  it('can filter by completed tasks', () => {
+    cy.visit('/todolists.html')
+
+    const listspage = new Listspage()
+    
+    listspage.createList("another list")
+    listspage.openList("another list")
+
+    const listpage = new Listpage()
+
+    listpage.createItem("my first task")
+    listpage.createItem("my second task")
+    listpage.createItem("my third task")
+    listpage.createItem("my fourth task")
+    listpage.createItem("my fifth task")
+
+    listpage.toggleItem("my second task")
+    listpage.toggleItem("my fourth task")
+
+    listpage.elements.completedFilter().click()
+
+    listpage.getItem("my second task").should('be.visible')
+    listpage.getItem("my fourth task").should('be.visible')
+    listpage.elements.listOfItems().should('not.contain.text', "my first task")
+    listpage.elements.listOfItems().should('not.contain.text', "my third task")
+    listpage.elements.listOfItems().should('not.contain.text', "my fifth task")
+  })
+})
